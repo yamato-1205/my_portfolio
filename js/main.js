e4a72fcd526d3080df40f38dba4d72a6d07a399f
yamato-1205 <ysakata1205@gmail.com>
2023-10-23 05:00:28 +0900

    趣味のページ作成

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function turn_one() {
+  document.getElementById("turn_one").classList.toggle("turn-arrow");
+}
+function turn_two() {
+  document.getElementById("turn_two").classList.toggle("turn-arrow");
+}
+function turn_three() {
+  document.getElementById("turn_three").classList.toggle("turn-arrow");
+}
